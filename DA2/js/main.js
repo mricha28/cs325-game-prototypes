f977dd2259f5951037cbc2433ffc013f5d5752fe
mrichardsnh <36209350+mrichardsnh@users.noreply.github.com>
2019-02-10 17:39:06 -0500

    Changes

--- DA2/js/main.js
+++ DA2/js/main.js
@@ -14,36 +14,127 @@ window.onload = function() {
     
     function preload() {
         // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+        game.load.image( 'Cursor_01', 'assets/Cursor_01.png' );
     }
     
-    var bouncy;
-    
+    var Cursor_01;
+    var keys;
     function create() {
         // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
+        Cursor_01 = game.add.sprite( game.world.centerX, game.world.centerY, 'Cursor_01' );
+        
         // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+        Cursor_01.anchor.setTo( 0.5, 0.5 );
+
+        game.physics.arcade.enable(Cursor_01);
+
+        Cursor_01.body.collideWorldBounds = true;
+
+        keys = game.input.keyboard.createCursorKeys();
     }
     
     function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
+    
+        player1Move();
+        playerRotation();
+        
+    }
+    
+    function toDegrees (angle) 
+    {
+        return angle * (180 / Math.PI);
+    }
+    
+    function playerRotation()
+    {
+        var angle = toDegrees(Math.atan(Cursor_01.body.velocity.y/Cursor_01.body.velocity.x));
+        console.log("Angle = "+angle);
+        //Check which quadrant cursor is in
+        if(Cursor_01.body.velocity.x > 0 && Cursor_01.body.velocity.y < 0)
+        {
+            Cursor_01.angle = angle;
+        }
+        else if(Cursor_01.body.velocity.x < 0 && Cursor_01.body.velocity.y < 0)
+        {
+            Cursor_01.angle = angle*-1;
+        }
+        else if(Cursor_01.body.velocity.x < 0 && Cursor_01.body.velocity.y > 0)
+        {
+            Cursor_01.angle = -90 - angle;
+        }
+        else if(Cursor_01.body.velocity.x > 0 && Cursor_01.body.velocity.y > 0)
+        {
+            Cursor_01.angle = 90 + angle;
+        }
+    }
+
+    //Set players velocity based on keyboard input
+    function player1Move()
+    {
+        
+        if (keys.left.isDown)
+        {
+            if(Cursor_01.body.velocity.x > 0)
+            {
+             Cursor_01.body.velocity.x -= 12;
+            }
+            else if(Cursor_01.body.velocity.x > -250)
+            {
+            Cursor_01.body.velocity.x -= 6;
+            }
+        }
+        else
+        {
+         Cursor_01.body.velocity.x += 3;   
+        }
+        
+        if(keys.right.isDown)
+        {
+            if(Cursor_01.body.velocity.x < 0)
+            {
+             Cursor_01.body.velocity.x += 12;
+            }
+            else if(Cursor_01.body.velocity.x < 250)
+            {
+            Cursor_01.body.velocity.x += 6;
+            }
+        } 
+        else
+        {
+         Cursor_01.body.velocity.x -= 3;   
+        }
+
+        if(keys.down.isDown)
+        {
+            if(Cursor_01.body.velocity.y < 0)
+            {
+             Cursor_01.body.velocity.y += 12;
+            }
+            else if(Cursor_01.body.velocity.y < 250)
+            {
+            Cursor_01.body.velocity.y += 6;
+            }
+        }
+        else
+        {
+         Cursor_01.body.velocity.y -= 3;   
+        }
+        
+        if(keys.up.isDown)
+        {
+            if(Cursor_01.body.velocity.y > 0)
+            {
+             Cursor_01.body.velocity.y -= 12;
+            }
+            else if(Cursor_01.body.velocity.y > -250)
+            {
+             Cursor_01.body.velocity.y -= 6;
+            }
+        }
+        else
+        {
+         Cursor_01.body.velocity.y += 3;   
+        }
     }
 };
